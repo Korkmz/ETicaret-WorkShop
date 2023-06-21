a9be2e0e25ebca1c0146fde12b7ad883b06c8d68
hatice korkmaz <hatice.korkmaz@intimeinfo.com.tr>
2023-06-21 11:51:07 +0300

    baseEntity created

---
+++ Core/ETicaretAPI.Application/Abstractions/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using ETicaretAPI.Domain.Entities;
+
+namespace ETicaretAPI.Application.Abstractions
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productService.GetProducts());
+        }
+    }
+}
